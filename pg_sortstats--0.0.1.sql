6a76e224cbee07d4e3769e3dbd1da9ce79ea1954
Julien Rouhaud <julien.rouhaud@free.fr>
2018-07-25 00:45:41 +0200

    Aggregate counters in shared memory.

--- pg_sortstats--0.0.1.sql
+++ pg_sortstats--0.0.1.sql
@@ -8,3 +8,25 @@
 
 SET client_encoding = 'UTF8';
 
+CREATE FUNCTION pg_sortstats(
+    OUT queryid bigint,
+    OUT userid oid,
+    OUT dbid oid,
+    OUT sort_key text,
+    OUT lines bigint,
+    OUT lines_to_sort bigint,
+    OUT work_mems bigint,
+    OUT topn_sorts bigint,
+    OUT quicksorts bigint,
+    OUT external_sorts bigint,
+    OUT external_merges bigint,
+    OUT nbtapes bigint
+)
+    RETURNS SETOF record
+LANGUAGE c COST 1000
+AS '$libdir/pg_sortstats', 'pg_sortstats';
+
+CREATE FUNCTION pg_sortstats_reset()
+    RETURNS void
+LANGUAGE c COST 1000
+AS '$libdir/pg_sortstats', 'pg_sortstats_reset';
--- pg_sortstats.c
+++ pg_sortstats.c
@@ -13,9 +13,12 @@
 #include "fmgr.h"
 #include "funcapi.h"
 #include "miscadmin.h"
+#include "access/hash.h"
 #include "nodes/nodeFuncs.h"
 #include "parser/parsetree.h"
 #include "storage/ipc.h"
+#include "storage/lwlock.h"
+#include "storage/shmem.h"
 #include "utils/builtins.h"
 #include "utils/guc.h"
 #if PG_VERSION_NUM >= 90500
@@ -28,7 +31,54 @@
 PG_MODULE_MAGIC;
 
 /*--- Macros and structs ---*/
-#define PGSRT_COLUMNS 3			/* number of columns in pg_sortstats  SRF */
+#define PGSRT_COLUMNS		12			/* number of columns in pg_sortstats  SRF */
+#define PGSRT_KEYS_SIZE		80
+#define USAGE_DECREASE_FACTOR	(0.99)	/* decreased every pgsrt_entry_dealloc */
+#define USAGE_DEALLOC_PERCENT	5		/* free this % of entries at once */
+#define USAGE_INIT	(1.0)
+
+/* In PostgreSQL 11, queryid becomes a uint64 internally.
+ */
+#if PG_VERSION_NUM >= 110000
+typedef uint64 pgsrt_queryid;
+#else
+typedef uint32 pgsrt_queryid;
+#endif
+
+typedef struct pgsrtSharedState
+{
+	LWLockId	lock;					/* protects hashtable search/modification */
+	double		cur_median_usage;		/* current median usage in hashtable */
+} pgsrtSharedState;
+
+typedef struct pgsrtHashKey
+{
+	Oid				userid;			/* user OID */
+	Oid				dbid;			/* database OID */
+	pgsrt_queryid	queryid;		/* query identifier */
+	uint32			sortid;			/* sort identifier withing a query */
+} pgsrtHashKey;
+
+typedef struct pgsrtCounters
+{
+	double			usage;					/* usage factor */
+	int64			lines;					/* total number of lines in input */
+	int64			lines_to_sort;			/* total number of lines sorted */
+	int64			work_mems;				/* total size of estimated work_mem */
+	int64			topn_sorts;				/* number of top-N heapsorts */
+	int64			quicksorts;				/* number of quicksorts */
+	int64			external_sorts;			/* number of external sorts */
+	int64			external_merges;		/* number of external merges */
+	int64			nbtapes;
+	char			keys[PGSRT_KEYS_SIZE];	/* deparsed sort key */
+} pgsrtCounters;
+
+typedef struct pgsrtEntry
+{
+	pgsrtHashKey	key;
+	pgsrtCounters	counters;	/* statistics for this sort */
+	slock_t			mutex;				/* protects the counters only */
+} pgsrtEntry;
 
 typedef struct pgsrtWalkerContext
 {
@@ -45,10 +95,14 @@ void		_PG_init(void);
 void		_PG_fini(void);
 
 
-extern Datum pg_sortstats(PG_FUNCTION_ARGS);
+extern PGDLLEXPORT Datum	pg_sortstats(PG_FUNCTION_ARGS);
+extern PGDLLEXPORT Datum	pg_sortstats_reset(PG_FUNCTION_ARGS);
+
 PG_FUNCTION_INFO_V1(pg_sortstats);
+PG_FUNCTION_INFO_V1(pg_sortstats_reset);
 
 static void pgsrt_shmem_startup(void);
+static void pgsrt_shmem_shutdown(int code, Datum arg);
 static void pgsrt_ExecutorStart(QueryDesc *queryDesc, int eflags);
 static void pgsrt_ExecutorRun(QueryDesc *queryDesc,
 				 ScanDirection direction,
@@ -70,6 +124,15 @@ static ExecutorRun_hook_type prev_ExecutorRun = NULL;
 static ExecutorFinish_hook_type prev_ExecutorFinish = NULL;
 static ExecutorEnd_hook_type prev_ExecutorEnd = NULL;
 
+static Size pgsrt_memsize(void);
+
+static pgsrtEntry *pgsrt_entry_alloc(pgsrtHashKey *key, char *keys);
+static void pgsrt_entry_dealloc(void);
+static void pgsrt_entry_reset(void);
+static void pgsrt_entry_store(pgsrt_queryid queryId, pgsrtCounters *counters);
+static uint32 pgsrt_hash_fn(const void *key, Size keysize);
+static int	pgsrt_match_fn(const void *key1, const void *key2, Size keysize);
+
 static bool pgsrt_planstate_walker(PlanState *ps, pgsrtWalkerContext *context);
 static char * pgsrt_get_sort_group_keys(PlanState *planstate,
 					 int nkeys, AttrNumber *keycols,
@@ -80,32 +143,20 @@ static void pgsrt_setup_walker_context(pgsrtWalkerContext *context);
 /*--- Local variables ---*/
 static int nesting_level = 0;
 static bool pgsrt_enabled;
-//static int pgsrt_max;
+static int pgsrt_max;
 
-/* Global Hash */
-//static HTAB *pgsrt_hash = NULL;
-//static pgsrtSharedState *pgsrt = NULL;
+static HTAB *pgsrt_hash = NULL;
+static pgsrtSharedState *pgsrt = NULL;
 
 
 void
 _PG_init(void)
 {
-	//if (!process_shared_preload_libraries_in_progress)
-	//{
-	//	elog(ERROR, "This module can only be loaded via shared_preload_libraries");
-	//	return;
-	//}
-
-	prev_ExecutorStart = ExecutorStart_hook;
-	ExecutorStart_hook = pgsrt_ExecutorStart;
-	prev_ExecutorRun = ExecutorRun_hook;
-	ExecutorRun_hook = pgsrt_ExecutorRun;
-	prev_ExecutorFinish = ExecutorFinish_hook;
-	ExecutorFinish_hook = pgsrt_ExecutorFinish;
-	prev_ExecutorEnd = ExecutorEnd_hook;
-	ExecutorEnd_hook = pgsrt_ExecutorEnd;
-	prev_shmem_startup_hook = shmem_startup_hook;
-	shmem_startup_hook = pgsrt_shmem_startup;
+	if (!process_shared_preload_libraries_in_progress)
+	{
+		elog(ERROR, "This module can only be loaded via shared_preload_libraries");
+		return;
+	}
 
 	DefineCustomBoolVariable("pg_sortstats.enabled",
 							 "Enable / Disable pg_sortstats",
@@ -118,23 +169,98 @@ _PG_init(void)
 							 NULL,
 							 NULL);
 
-	//DefineCustomIntVariable("pg_sortstats.max",
-	//						"Sets the maximum number of statements tracked by pg_sortstats.",
-	//						NULL,
-	//						&pgsrt_max,
-	//						1000,
-	//						100,
-	//						INT_MAX,
-	//						PGC_POSTMASTER,
-	//						0,
-	//						NULL,
-	//						NULL,
-	//						NULL);
+	DefineCustomIntVariable("pg_sortstats.max",
+							"Sets the maximum number of statements tracked by pg_sortstats.",
+							NULL,
+							&pgsrt_max,
+							1000,
+							100,
+							INT_MAX,
+							PGC_POSTMASTER,
+							0,
+							NULL,
+							NULL,
+							NULL);
+
+	RequestAddinShmemSpace(pgsrt_memsize());
+#if PG_VERSION_NUM >= 90600
+	RequestNamedLWLockTranche("pg_sortstats", 1);
+#else
+	RequestAddinLWLocks(1);
+#endif
+
+	/* install hooks */
+	prev_ExecutorStart = ExecutorStart_hook;
+	ExecutorStart_hook = pgsrt_ExecutorStart;
+	prev_ExecutorRun = ExecutorRun_hook;
+	ExecutorRun_hook = pgsrt_ExecutorRun;
+	prev_ExecutorFinish = ExecutorFinish_hook;
+	ExecutorFinish_hook = pgsrt_ExecutorFinish;
+	prev_ExecutorEnd = ExecutorEnd_hook;
+	ExecutorEnd_hook = pgsrt_ExecutorEnd;
+	prev_shmem_startup_hook = shmem_startup_hook;
+	shmem_startup_hook = pgsrt_shmem_startup;
 }
 
 static void
 pgsrt_shmem_startup(void)
 {
+	bool		found;
+	HASHCTL		info;
+
+	if (prev_shmem_startup_hook)
+		prev_shmem_startup_hook();
+
+	/* reset in case this is a restart within the postmaster */
+	pgsrt = NULL;
+
+	/* Create or attach to the shared memory state */
+	LWLockAcquire(AddinShmemInitLock, LW_EXCLUSIVE);
+
+	/* global access lock */
+	pgsrt = ShmemInitStruct("pg_sortstats",
+					sizeof(pgsrtSharedState),
+					&found);
+
+	if (!found)
+	{
+		/* First time through ... */
+#if PG_VERSION_NUM >= 90600
+		pgsrt->lock = &(GetNamedLWLockTranche("pg_sortstats"))->lock;
+#else
+		pgsrt->lock = LWLockAssign();
+#endif
+	}
+
+	memset(&info, 0, sizeof(info));
+	info.keysize = sizeof(pgsrtHashKey);
+	info.entrysize = sizeof(pgsrtEntry);
+	info.hash = pgsrt_hash_fn;
+	info.match = pgsrt_match_fn;
+
+	/* allocate stats shared memory hash */
+	pgsrt_hash = ShmemInitHash("pg_sortstats hash",
+							  pgsrt_max, pgsrt_max,
+							  &info,
+							  HASH_ELEM | HASH_FUNCTION | HASH_COMPARE);
+
+	LWLockRelease(AddinShmemInitLock);
+
+	if (!IsUnderPostmaster)
+		on_shmem_exit(pgsrt_shmem_shutdown, (Datum) 0);
+
+	/*
+	 * Done if some other process already completed our initialization.
+	 */
+	if (found)
+		return;
+}
+
+/* Save the statistics into a file at shutdown */
+static void
+pgsrt_shmem_shutdown(int code, Datum arg)
+{
+	/* TODO */
 }
 
 void
@@ -249,10 +375,221 @@ pgsrt_ExecutorEnd(QueryDesc *queryDesc)
 		standard_ExecutorEnd(queryDesc);
 }
 
+static Size
+pgsrt_memsize(void)
+{
+	Size	size;
+
+	size = MAXALIGN(sizeof(pgsrtSharedState));
+	size = add_size(size, hash_estimate_size(pgsrt_max, sizeof(pgsrtEntry)));
+
+	return size;
+}
+
+/*
+ * Allocate a new hashtable entry.
+ * caller must hold an exclusive lock on pgsrt->lock
+ */
+static pgsrtEntry *
+pgsrt_entry_alloc(pgsrtHashKey *key, char *keys)
+{
+	pgsrtEntry *entry;
+	bool		found;
+
+	/* Make space if needed */
+	while (hash_get_num_entries(pgsrt_hash) >= pgsrt_max)
+		pgsrt_entry_dealloc();
+
+	/* Find or create an entry with desired hash code */
+	entry = (pgsrtEntry *) hash_search(pgsrt_hash, key, HASH_ENTER, &found);
+
+	if (!found)
+	{
+		/* New entry, initialize it */
+
+		/* reset the statistics */
+		memset(&entry->counters, 0, sizeof(pgsrtCounters));
+		/* set the appropriate initial usage count */
+		entry->counters.usage = USAGE_INIT;
+		memcpy(entry->counters.keys, keys, PGSRT_KEYS_SIZE - 1);
+		/* re-initialize the mutex each time ... we assume no one using it */
+		SpinLockInit(&entry->mutex);
+	}
+
+	return entry;
+}
+
+/*
+ * qsort comparator for sorting into increasing usage order
+ */
+static int
+entry_cmp(const void *lhs, const void *rhs)
+{
+	double		l_usage = (*(pgsrtEntry *const *) lhs)->counters.usage;
+	double		r_usage = (*(pgsrtEntry *const *) rhs)->counters.usage;
+
+	if (l_usage < r_usage)
+		return -1;
+	else if (l_usage > r_usage)
+		return +1;
+	else
+		return 0;
+}
+
+/*
+ * Deallocate least used entries.
+ * Caller must hold an exclusive lock on pgsrt->lock.
+ */
+static void
+pgsrt_entry_dealloc(void)
+{
+	HASH_SEQ_STATUS hash_seq;
+	pgsrtEntry **entries;
+	pgsrtEntry  *entry;
+	int			nvictims;
+	int			i;
+
+	/*
+	 * Sort entries by usage and deallocate USAGE_DEALLOC_PERCENT of them.
+	 * While we're scanning the table, apply the decay factor to the usage
+	 * values.
+	 */
+	entries = palloc(hash_get_num_entries(pgsrt_hash) * sizeof(pgsrtEntry *));
+
+	i = 0;
+	hash_seq_init(&hash_seq, pgsrt_hash);
+	while ((entry = hash_seq_search(&hash_seq)) != NULL)
+	{
+		entries[i++] = entry;
+		entry->counters.usage *= USAGE_DECREASE_FACTOR;
+	}
+
+	qsort(entries, i, sizeof(pgsrtEntry *), entry_cmp);
+
+	if (i > 0)
+	{
+		/* Record the (approximate) median usage */
+		pgsrt->cur_median_usage = entries[i / 2]->counters.usage;
+	}
+
+	nvictims = Max(10, i * USAGE_DEALLOC_PERCENT / 100);
+	nvictims = Min(nvictims, i);
+
+	for (i = 0; i < nvictims; i++)
+	{
+		hash_search(pgsrt_hash, &entries[i]->key, HASH_REMOVE, NULL);
+	}
+
+	pfree(entries);
+}
+
+/* Remove all saved entries in shmem */
+static void
+pgsrt_entry_reset(void)
+{
+	HASH_SEQ_STATUS hash_seq;
+	pgsrtEntry  *entry;
+
+	LWLockAcquire(pgsrt->lock, LW_EXCLUSIVE);
+
+	hash_seq_init(&hash_seq, pgsrt_hash);
+	while ((entry = hash_seq_search(&hash_seq)) != NULL)
+	{
+		hash_search(pgsrt_hash, &entry->key, HASH_REMOVE, NULL);
+	}
+
+	LWLockRelease(pgsrt->lock);
+}
+
+static void
+pgsrt_entry_store(pgsrt_queryid queryId, pgsrtCounters *counters)
+{
+	volatile pgsrtEntry *e;
+
+	pgsrtHashKey key;
+	pgsrtEntry  *entry;
+
+	/* Safety check... */
+	if (!pgsrt || !pgsrt_hash)
+		return;
+
+	/* Set up key for hashtable search */
+	key.userid = GetUserId();
+	key.dbid = MyDatabaseId;
+	key.queryid = queryId;
+	key.sortid = (uint32) hash_any((unsigned char *) counters->keys,
+			strlen(counters->keys));
+
+	/* Lookup the hash table entry with shared lock. */
+	LWLockAcquire(pgsrt->lock, LW_SHARED);
+
+	entry = (pgsrtEntry *) hash_search(pgsrt_hash, &key, HASH_FIND, NULL);
+
+	/* Create new entry, if not present */
+	if (!entry)
+	{
+		/* Need exclusive lock to make a new hashtable entry - promote */
+		LWLockRelease(pgsrt->lock);
+		LWLockAcquire(pgsrt->lock, LW_EXCLUSIVE);
+
+		/* OK to create a new hashtable entry */
+		entry = pgsrt_entry_alloc(&key, counters->keys);
+	}
+
+	/*
+	 * Grab the spinlock while updating the counters */
+	e = (volatile pgsrtEntry *) entry;
+
+	SpinLockAcquire(&e->mutex);
+
+	e->counters.usage += 1;
+	e->counters.lines += counters->lines;
+	e->counters.lines_to_sort += counters->lines_to_sort;
+	e->counters.work_mems += counters->work_mems;
+	e->counters.topn_sorts += counters->topn_sorts;
+	e->counters.quicksorts += counters->quicksorts;
+	e->counters.external_sorts += counters->external_sorts;
+	e->counters.external_merges += counters->external_merges;
+	e->counters.nbtapes += counters->nbtapes;
+
+	SpinLockRelease(&e->mutex);
+
+	LWLockRelease(pgsrt->lock);
+}
+
+/* Compute hash value for a pgsrtHashKey.  sortid is already hashed */
+static uint32
+pgsrt_hash_fn(const void *key, Size keysize)
+{
+	const pgsrtHashKey *k = (const pgsrtHashKey *) key;
+
+	return hash_uint32((uint32) k->userid) ^
+		hash_uint32((uint32) k->dbid) ^
+		hash_uint32((uint32) k->queryid) ^
+		k->sortid;
+}
+
+/* Compare two pgsrtHashKey keys.  Zero means match */
+static int
+pgsrt_match_fn(const void *key1, const void *key2, Size keysize)
+{
+	const pgsrtHashKey *k1 = (const pgsrtHashKey *) key1;
+	const pgsrtHashKey *k2 = (const pgsrtHashKey *) key2;
+
+	if (k1->userid == k2->userid &&
+		k1->dbid == k2->dbid &&
+		k1->queryid == k2->queryid &&
+		k1->sortid == k2->sortid)
+		return 0;
+	else
+		return 1;
+}
+
 static bool pgsrt_planstate_walker(PlanState *ps, pgsrtWalkerContext *context)
 {
 	if (IsA(ps, SortState))
 	{
+		pgsrtCounters counters;
 		Plan *plan = ps->plan;
 		SortState *srtstate = (SortState *) ps;
 		Tuplesortstate *state = (Tuplesortstate *) srtstate->tuplesortstate;
@@ -270,7 +607,7 @@ static bool pgsrt_planstate_walker(PlanState *ps, pgsrtWalkerContext *context)
 		int64 lines_to_sort;
 		int tuple_palloc;
 		int i = 1;
-		long w_m;
+		int64 w_m;
 
 		if (state)
 		{
@@ -341,39 +678,64 @@ static bool pgsrt_planstate_walker(PlanState *ps, pgsrtWalkerContext *context)
 				nbtapes = ((struct pgsrt_Tuplesortstate *) state)->currentRun + 1;
 			}
 
-			elog(WARNING, "sort info:\n"
-					"keys: %s\n"
-					"type: %s\n"
-					"space type: %s\n"
-					"space: %ld kB\n"
-					"w_m estimated: %ld kB\n"
-					"nbTapes: %d\n"
-#if PG_VERSION_NUM >= 110000
-					"parallel: %s (%d)\n"
+			counters.lines = lines;
+			counters.lines_to_sort = lines_to_sort;
+			counters.work_mems = w_m;
+			if (stats.sortMethod == SORT_TYPE_TOP_N_HEAPSORT)
+				counters.topn_sorts = 1;
+			else
+				counters.topn_sorts = 0;
+
+			if (stats.sortMethod == SORT_TYPE_QUICKSORT)
+				counters.quicksorts = 1;
+			else
+				counters.quicksorts = 0;
+
+			if (stats.sortMethod == SORT_TYPE_EXTERNAL_SORT)
+				counters.external_sorts = 1;
+			else
+				counters.external_sorts = 0;
+
+			if (stats.sortMethod == SORT_TYPE_EXTERNAL_MERGE)
+				counters.external_merges = 1;
+			else
+				counters.external_merges = 0;
+
+			counters.nbtapes = nbtapes;
+			memset(counters.keys, 0, PGSRT_KEYS_SIZE);
+			memcpy(counters.keys, deparsed, PGSRT_KEYS_SIZE - 1);
+
+			pgsrt_entry_store(context->queryDesc->plannedstmt->queryId, &counters);
+
+			//elog(WARNING, "sort info:\n"
+			//		"keys: %s\n"
+			//		"type: %s\n"
+			//		"space type: %s\n"
+			//		"space: %ld kB\n"
+			//		"lines to sort: %ld\n"
+			//		"w_m estimated: %ld kB\n"
+			//		"nbTapes: %d\n"
+#if PG_VERSI//ON_NUM >= 110000
+			//		"parallel: %s (%d)\n"
 #endif
-					"bounded? %s - bound %ld - %ld",
-					deparsed,
-					sortMethod,
-					spaceType,
-					spaceUsed,
-					w_m,
-					nbtapes,
-#if PG_VERSION_NUM >= 110000
-					(srtstate->shared_info ? "yes" : "no"),(srtstate->shared_info ? srtstate->shared_info->num_workers : -1),
+			//		"bounded? %s - %s , bound %ld - %ld",
+			//		deparsed,
+			//		sortMethod,
+			//		spaceType,
+			//		spaceUsed,
+			//		lines_to_sort,
+			//		w_m,
+			//		nbtapes,
+#if PG_VERSI//ON_NUM >= 110000
+			//		(srtstate->shared_info ? "yes" : "no"),(srtstate->shared_info ? srtstate->shared_info->num_workers : -1),
 #endif
-					(srtstate->bounded ? "yes":"no"), srtstate->bound, srtstate->bound_Done);
+			//		(srtstate->bounded ? "yes":"no"),(srtstate->bounded_Done ? "yes":"no"), srtstate->bound, srtstate->bound_Done);
 		}
 	}
 
 	context->ancestors = lcons(ps, context->ancestors);
 
 	return planstate_tree_walker(ps, pgsrt_planstate_walker, context);
-}
-
-Datum
-pg_sortstats(PG_FUNCTION_ARGS)
-{
-	return (Datum) 0;
 }
 
 /* Adapted from ExplainPrintPlan */
@@ -447,4 +809,108 @@ pgsrt_get_sort_group_keys(PlanState *planstate,
 	}
 
 	return sortkeybuf.data;
+}
+
+/*
+ * Reset statistics.
+ */
+PGDLLEXPORT Datum
+pg_sortstats_reset(PG_FUNCTION_ARGS)
+{
+	if (!pgsrt)
+		ereport(ERROR,
+				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
+				 errmsg("pg_sortstats must be loaded via shared_preload_libraries")));
+
+	pgsrt_entry_reset();
+	PG_RETURN_VOID();
+}
+
+Datum
+pg_sortstats(PG_FUNCTION_ARGS)
+{
+	ReturnSetInfo	*rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+	MemoryContext	per_query_ctx;
+	MemoryContext	oldcontext;
+	TupleDesc		tupdesc;
+	Tuplestorestate	*tupstore;
+	HASH_SEQ_STATUS hash_seq;
+	pgsrtEntry		*entry;
+
+
+	if (!pgsrt)
+		ereport(ERROR,
+				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
+				 errmsg("pg_stat_kcache must be loaded via shared_preload_libraries")));
+	/* check to see if caller supports us returning a tuplestore */
+	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("set-valued function called in context that cannot accept a set")));
+	if (!(rsinfo->allowedModes & SFRM_Materialize))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("materialize mode required, but it is not " \
+							"allowed in this context")));
+
+	/* Switch into long-lived context to construct returned data structures */
+	per_query_ctx = rsinfo->econtext->ecxt_per_query_memory;
+	oldcontext = MemoryContextSwitchTo(per_query_ctx);
+
+	/* Build a tuple descriptor for our result type */
+	if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+		elog(ERROR, "return type must be a row type");
+
+	tupstore = tuplestore_begin_heap(true, false, work_mem);
+	rsinfo->returnMode = SFRM_Materialize;
+	rsinfo->setResult = tupstore;
+	rsinfo->setDesc = tupdesc;
+
+	MemoryContextSwitchTo(oldcontext);
+
+	LWLockAcquire(pgsrt->lock, LW_SHARED);
+
+	hash_seq_init(&hash_seq, pgsrt_hash);
+	while ((entry = hash_seq_search(&hash_seq)) != NULL)
+	{
+		Datum			values[PGSRT_COLUMNS];
+		bool			nulls[PGSRT_COLUMNS];
+		pgsrtCounters	tmp;
+		int				i = 0;
+
+		memset(values, 0, sizeof(values));
+		memset(nulls, 0, sizeof(nulls));
+
+		/* copy counters to a local variable to keep locking time short */
+		{
+			volatile pgsrtEntry *e = (volatile pgsrtEntry *) entry;
+
+			SpinLockAcquire(&e->mutex);
+			tmp = e->counters;
+			SpinLockRelease(&e->mutex);
+		}
+
+		values[i++] = Int64GetDatum(entry->key.queryid);
+		values[i++] = ObjectIdGetDatum(entry->key.userid);
+		values[i++] = ObjectIdGetDatum(entry->key.dbid);
+		values[i++] = CStringGetTextDatum(tmp.keys);
+		values[i++] = Int64GetDatumFast(tmp.lines);
+		values[i++] = Int64GetDatumFast(tmp.lines_to_sort);
+		values[i++] = Int64GetDatumFast(tmp.work_mems);
+		values[i++] = Int64GetDatumFast(tmp.topn_sorts);
+		values[i++] = Int64GetDatumFast(tmp.quicksorts);
+		values[i++] = Int64GetDatumFast(tmp.external_sorts);
+		values[i++] = Int64GetDatumFast(tmp.external_merges);
+		values[i++] = Int64GetDatumFast(tmp.nbtapes);
+
+		Assert(i == PGSRT_COLUMNS);
+
+		tuplestore_putvalues(tupstore, tupdesc, values, nulls);
+	}
+
+	LWLockRelease(pgsrt->lock);
+
+	/* clean up and return the tuplestore */
+	tuplestore_donestoring(tupstore);
+	return (Datum) 0;
 }
