c7c036dea67956dc93c52f73bcc200d617963332
Julien Rouhaud <julien.rouhaud@free.fr>
2018-08-04 21:51:34 +0200

    Make regression tests stable across releases

--- sql/pg_sortstats.sql
+++ sql/pg_sortstats.sql
@@ -13,10 +13,11 @@ SELECT * FROM sorts ORDER BY id DESC LIMIT 1;
 
 SELECT nb_keys, sort_keys, lines, lines_to_sort,
     work_mems < (11 * 1024) AS "exp_less_11MB",
-    topn_sorts, quicksorts, external_sorts, external_merges, nb_tapes,
+    topn_sorts, quicksorts, external_sorts, external_merges,
+    nb_tapes > 2 AS multiple_tapes,
     space_disk > 1024 AS "disk_more_1MB",
     space_memory > 1024 AS  "mem_more_1MB",
-    non_parallels, nb_workers
+    non_parallels, COALESCE(nb_workers, 0) AS nb_workers
 FROM pg_sortstats(true) ORDER BY nb_keys;
 
 SELECT * FROM pg_sortstats_reset();
@@ -29,10 +30,11 @@ SELECT * FROM src LIMIT 1;
 
 SELECT nb_keys, sort_keys, lines, lines_to_sort,
     work_mems < (11 * 1024) AS "exp_less_11MB",
-    topn_sorts, quicksorts, external_sorts, external_merges, nb_tapes,
+    topn_sorts, quicksorts, external_sorts, external_merges,
+    nb_tapes > 2 AS multiple_tapes,
     space_disk > 1024 AS "disk_more_1MB",
     space_memory > 1024 AS  "mem_more_1MB",
-    non_parallels, nb_workers
+    non_parallels, COALESCE(nb_workers, 0) AS nb_workers
 FROM pg_sortstats(true) ORDER BY nb_keys;
 
 SELECT * FROM pg_sortstats_reset();
